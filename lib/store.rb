3d787c19a5a333e25decb25f5088e5477fed8906
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-07 00:24:56 -0400

    Feat: add validations to both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality { only_integer: true, greater_than: 0}
+
+  def store_carries_mens_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, "cannot both be false")
+      errors.add(:womens_apparel, "cannot both be false")
+    end
+  end
+  
 end
