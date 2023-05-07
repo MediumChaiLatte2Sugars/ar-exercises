dfca1f08a47a37b22af3b2c773e237c09c01731d
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-06 23:06:31 -0400

    Feat: delete third store with verification

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,3 +8,8 @@ puts "----------"
 # Load third store, assign to variable
 @store3 = Store.find_by(id: 3)
 
+# Delete store from database
+@store3.destroy
+
+# Verfy count post delete
+puts Store.count
