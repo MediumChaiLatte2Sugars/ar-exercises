02607b457488ee05f685a45c3a43c70635020e27
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-07 00:35:13 -0400

    Fix: add missing semicolon

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
-  validates :annual_revenue, numericality { only_integer: true, greater_than: 0}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0}
 
   def store_carries_mens_or_womens_apparel
     unless mens_apparel || womens_apparel
