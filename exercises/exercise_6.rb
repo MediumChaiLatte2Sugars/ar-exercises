b7ac36164cfc14463af80c302f410d29d8eb2111
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-07 00:35:00 -0400

    Fix: update employee data to agree with rules

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,12 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Add data into employees
-@store1.employees.create(first_name: 'Orange', last_name: 'Julius', hourly_rate: 0)
+@store1.employees.create(first_name: 'Orange', last_name: 'Julius', hourly_rate: 47)
 @store1.employees.create(first_name: 'Eggward', last_name: 'Chicken', hourly_rate: 350)
-@store1.employees.create(first_name: 'Shrek', last_name: 'Andrews', hourly_rate: 37)
+@store1.employees.create(first_name: 'Shrek', last_name: 'Andrews', hourly_rate: 52)
 @store1.employees.create(first_name: 'Bogota', last_name: 'Seylon', hourly_rate: 100)
-@store2.employees.create(first_name: 'Karaoke', last_name: 'Machine', hourly_rate: 1000)
-@store2.employees.create(first_name: 'Erlenmeyer', last_name: 'Flask', hourly_rate: 1)
-@store2.employees.create(first_name: 'Anisakis', last_name: 'Gastrocrawler', hourly_rate: 500)
-@store2.employees.create(first_name: 'Orange', last_name: 'Julius II', hourly_rate: 0)
-@store2.employees.create(first_name: 'Orange', last_name: 'Julius III', hourly_rate: 0)
+@store2.employees.create(first_name: 'Karaoke', last_name: 'Machine', hourly_rate: 50)
+@store2.employees.create(first_name: 'Erlenmeyer', last_name: 'Flask', hourly_rate: 100)
+@store2.employees.create(first_name: 'Anisakis', last_name: 'Gastrocrawler', hourly_rate: 198)
+@store2.employees.create(first_name: 'Orange', last_name: 'Julius II', hourly_rate: 48)
+@store2.employees.create(first_name: 'Orange', last_name: 'Julius III', hourly_rate: 49)
